c189ae476291800a2556bc2315cabc0ef40be173
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2024-11-30 13:19:16 -0300

    DP-04 Create implementation for pet service method calls. Apply them. Clean up the imports.

--- 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,18 +1,12 @@
 package br.com.alura;
 
-import br.com.alura.client.ClientHttpConfiguration;
-import br.com.alura.service.AbrigoService;
-import br.com.alura.service.PetService;
-
 import java.util.Scanner;
 
 public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
-        ClientHttpConfiguration clientHttpConfiguration = new ClientHttpConfiguration();
-        PetService petService = new PetService(clientHttpConfiguration);
-
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+
         try {
             int opcaoEscolhida = 0;
             while (opcaoEscolhida != 5) {
@@ -33,9 +27,9 @@ public class AdopetConsoleApplication {
                 } else if (opcaoEscolhida == 2) {
                     executor.invoker(new CadastrarAbrigoCommand());
                 } else if (opcaoEscolhida == 3) {
-                    petService.listarPetsDoAbrigo();
+                    executor.invoker(new ListarPetsDoAbrigoCommand());
                 } else if (opcaoEscolhida == 4) {
-                    petService.importarPetsDoAbrigo();
+                    executor.invoker(new ImportarPetsDoAbrigoCommand());
                 } else if (opcaoEscolhida == 5) {
                     break;
                 } else {
